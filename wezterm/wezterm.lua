f7b0b3517316d38573bad0a572dac51fbdb59214
Fauzan Valdera <f.valdera@yahoo.co.id>
2025-03-02 14:37:08 +0700

    Add zsh and tmux config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,13 +8,13 @@ config.scrollback_lines = 5000
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 14
 
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config)
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.window_padding = { left = 20, right = 20, top = 55, bottom = 15 }
+config.window_padding = { left = 10, right = 10, top = 55, bottom = 0 }
 
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 30
